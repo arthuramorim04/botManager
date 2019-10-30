d65812ed75db1094a5f6b7059cbb74584f879e68
Arthur paulo <arthurpas67@gmail.com>
2019-10-30 18:35:06 -0300

    scrapy manager funcionando

--- manager/manager/spiders/managerbot.py
+++ manager/manager/spiders/managerbot.py
@@ -5,20 +5,26 @@ from manager.items import ManagerItem
 
 class ManagerbotSpider(scrapy.Spider):
     name = 'managerbot'
-    allowed_domains = ['manager.com.br/empregos-desenvolvedor-javascript']
-    start_urls = ['http://manager.com.br/empregos-desenvolvedor-javascript/']
+    allowed_domains = ['manager.com.br']
+    start_urls = ['http://manager.com.br/empregos-desenvolvedor-javascript/']            
 
     def parse(self, response):
 
-        items = response.xpath('.//div[contains(@class, "lista-resultado-busca)]//article]contains(@class, "vaga hlisting")')
+        items = response.xpath('//*[@id="resultado-busca-vagas"]')
+        #items = response.xpath('.//div[contains(@class, "lista-resultado-busca)]//article]contains(@class, "vaga_hlisting")')
         for item in items:
+
             titulo_vaga = item.xpath('.//a[contains(@class, "permalink")]/text()').extract_first() #Pega titulo real da vaga
             descricao = item.xpath('.//p[contains(@class, "descricao description")]/text()').extract()
-            salario = item.xpath('.//small[contains(@class, "faixa-salarial muted")]//title()').extract() #pega faixa salarial
+            salario = item.xpath('/html/body/div[4]/div/form/div[2]/section/div[2]/article[1]/header/small/span[1]').extract() #pega faixa salarial
             cidade = item.xpath('.//footer[contains(@class, "rodape")]//span[contains(@class, "localidade linha-rodape-vaga location adr")]//a[contains(@class, "cidade locality")]//span/text()').extract() #pega cidade
             estado = item.xpath('.//footer[contains(@class, "rodape")]//span[contains(@class, "localidade linha-rodape-vaga location adr")]//a[contains(@class, "uf region")]//span/text()').extract() #pega cidade
 
-        next_page_url = response.xpath('//div[contains(@class, "pagination pagination-centered hidden-print")]//li[contains(@title, Vagas de Emprego de Desenvolvedor Javascript - Página Seguinte")]/@href').extract()
-        if next_page_url:
-            next_page = 'http://manager.com.br/empregos-desenvolvedor-javascript/' + next_page_url
-            yield scrapy.Request(url = next_page, callback = self.parse)
+    
+        self.log(titulo_vaga)
+        self.log(descricao)
+        self.log(salario)
+        self.log(cidade)
+        self.log(estado)
+
+
