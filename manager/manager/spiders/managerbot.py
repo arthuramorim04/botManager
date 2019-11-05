ccd34f10611bad7652aaebfba524a4f7b81fd575
Arthur paulo <arthurpas67@gmail.com>
2019-11-05 18:47:11 -0300

    falta arrumar o nextpage

--- manager/manager/spiders/managerbot.py
+++ manager/manager/spiders/managerbot.py
@@ -16,7 +16,7 @@ class ManagerbotSpider(scrapy.Spider):
             url = item.xpath('.//header[contains(@class, "titulo")]//h2//a[contains(@class, "permalink")]/@href').extract_first()
             yield scrapy.Request(url=url, callback=self.parse_detalhes)
 
-            next_page = response.xpath('//div[contains(@class, "pagination pagination-centered hidden-print)]//ul//li[9]//a/@href').extract_first()
+            next_page = response.xpath('//div[contains(@class, "pagination pagination-centered hidden-print)]//ul//li[contains(@title, "Vagas de Emprego de Desenvolvedor Javascript - Página Seguinte")]//a/@href').extract_first()
             
             if next_page:
                 self.log('PRoxima pagina: {0}'.format(next_page))
@@ -26,11 +26,11 @@ class ManagerbotSpider(scrapy.Spider):
 
     def parse_detalhes(self, response):
 
-        items = response.xpath('//div[contains(@id, "conteudo-principal")]//div[contains(@class, "row)]//div[contains(@class, "span8")]//article[contains(@class, "vaga hlisting")]')
-        titulo = response.xpath('//header[contains(@class, "page-header")]//h1[contains(@class, "pull-left item offer announce fn")]/text()').extract_first()
-        desc = response.xpath('/div[2]//div[contains(@class, "description")]//p/text()').extract_first()
-        salario = response.xpath('//div[1]//dl//dd/text()').extract_first()
-        local = response.xpath('//div[1]//dl[contains(@class, "location adr")]//dd[contains(@class, "clear-none")]//span[contains(@class, "locality")]//text()').extract_first()
+        items = response.xpath('//*[contains(@class, "vaga hlisting")]')
+        titulo = items.xpath('.//header[contains(@class, "page-header")]//h1[contains(@class, "pull-left item offer announce fn")]/text()').extract_first()
+        desc = items.xpath('.//div[2]//div[contains(@class, "description")]//p/text()').extract_first()
+        salario = items.xpath('.//div[1]//dl//dd/text()').extract_first()
+        local = items.xpath('.//div[1]//dl[contains(@class, "location adr")]//dd[contains(@class, "clear-none")]//span[contains(@class, "locality")]//text()').extract_first()
         
         yield {
             'titulo ': titulo,
