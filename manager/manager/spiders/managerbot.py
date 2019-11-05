18cfe954e0df27b8f08de8e31cb81696216fe8f2
Arthur paulo <arthurpas67@gmail.com>
2019-11-05 18:40:35 -0300

    nao esta gerando logs, corrigir hoje

--- manager/manager/spiders/managerbot.py
+++ manager/manager/spiders/managerbot.py
@@ -5,7 +5,7 @@ import scrapy
 class ManagerbotSpider(scrapy.Spider):
     name = 'managerbot'
     allowed_domains = ['manager.com.br']
-    start_urls = ['http://manager.com.br/empregos-desenvolvedor-javascript/']            
+    start_urls = ['http://manager.com.br/empregos-desenvolvedor-javascript/']
 
     def parse(self, response):
 
@@ -13,17 +13,28 @@ class ManagerbotSpider(scrapy.Spider):
         #items = response.xpath('.//div[contains(@class, "lista-resultado-busca)]//article]contains(@class, "vaga_hlisting")')
         for item in items:
 
-            titulo = item.xpath('.//header[contains(@class, "titulo")]//h2//a[contains(@class, "permalink")]//mark/text()').extract()
-            descricao = item.xpath('.//p[contains(@class, "descricao description")]/text()').extract()
-            salario = item.xpath('.//header[contains(@class, "titulo")]//small[contains(@class, "pull-right")]//span/text()').extract() #pega faixa salarial
-            cidade = item.xpath('.//footer[contains(@class, "rodape")]//span[contains(@class, "localidade linha-rodape-vaga location adr")]//a[contains(@class, "cidade locality")]//span/text()').extract() #pega cidade
-            estado = item.xpath('.//footer[contains(@class, "rodape")]//span[contains(@class, "localidade linha-rodape-vaga location adr")]//a[contains(@class, "uf region")]//span/text()').extract() #pega cidade
-        
-        
-        self.log(titulo)
-        self.log(descricao)
-        self.log(salario)
-        self.log(cidade)
-        self.log(estado)
+            url = item.xpath('.//header[contains(@class, "titulo")]//h2//a[contains(@class, "permalink")]/@href').extract_first()
+            yield scrapy.Request(url=url, callback=self.parse_detalhes)
 
+            next_page = response.xpath('//div[contains(@class, "pagination pagination-centered hidden-print)]//ul//li[9]//a/@href').extract_first()
+            
+            if next_page:
+                self.log('PRoxima pagina: {0}'.format(next_page))
+                yield scrapy.Request(url=next_page, callback=self.parse)
 
+            
+
+    def parse_detalhes(self, response):
+
+        items = response.xpath('//div[contains(@id, "conteudo-principal")]//div[contains(@class, "row)]//div[contains(@class, "span8")]//article[contains(@class, "vaga hlisting")]')
+        titulo = response.xpath('//header[contains(@class, "page-header")]//h1[contains(@class, "pull-left item offer announce fn")]/text()').extract_first()
+        desc = response.xpath('/div[2]//div[contains(@class, "description")]//p/text()').extract_first()
+        salario = response.xpath('//div[1]//dl//dd/text()').extract_first()
+        local = response.xpath('//div[1]//dl[contains(@class, "location adr")]//dd[contains(@class, "clear-none")]//span[contains(@class, "locality")]//text()').extract_first()
+        
+        yield {
+            'titulo ': titulo,
+            'salario': salario,
+            'local' : local,
+            'descricao' : desc
+        }
